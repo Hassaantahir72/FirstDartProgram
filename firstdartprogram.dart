6b74f0ac6471d2a82cfec0feaab46a05dee4cead
MUHAMMAD HASSAAN TAHIR <hassaantahir991@gmail.com>
2024-11-30 13:24:39 +0500

    Create firstdartprogram.dart

---
+++ firstdartprogram.dart
@@ -0,0 +1,10 @@
+void main() {
+  // My first dart program
+
+  print('Welcome to Dart');
+  print('My name is Hassaan');
+
+  // we use \n to represents a newline character
+
+  print('hello world\nMy first program');
+}
